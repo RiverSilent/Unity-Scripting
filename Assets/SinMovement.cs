95817a62248d519fe971d180ab601b9cb21f880f
unknown <prateergamer@gmail.com>
2018-04-13 11:37:15 +0530

    Add script to simulate sine function motion of a game object.

---
+++ Assets/SinMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SinMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(0.0f, 5.0f + Mathf.Sin(5.0f * Time.time), 0.0f);
+	}
+}
